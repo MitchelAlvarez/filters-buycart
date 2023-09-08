1df0d89c3ef5d3afcf30c7a1407fe06320102e88
Mitchel Alvarez <mitchel.alvarez.a@gmail.com>
2023-09-08 15:31:54 -0600

    Set: Add moks and icons

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+const root = createRoot(document.getElementById('app'))
+root.render(
+    <>
+        <h1>Shopping Cart</h1>
+        <h1>Shopping Cart</h1>
+    </>
+)
